2a740aa73d52d96643ec17d4e745a4048dad11d4
Suzukaze <1027743497@qq.com>
2024-05-31 13:12:16 +0800

    add codeforces hack

--- hack_automaton.py
+++ hack_automaton.py
@@ -1,14 +1,12 @@
 import shutil
 import subprocess
-from concurrent.futures import ThreadPoolExecutor, wait
+from concurrent.futures import Future, ThreadPoolExecutor, wait
 from multiprocessing import cpu_count
 from pathlib import Path
-from requests import get
+from typing import Callable, Iterable
+from requests import get, post
 from bs4 import BeautifulSoup
 from func_timeout import FunctionTimedOut, func_set_timeout
-
-THREAD_COUNT = max(1, cpu_count() // 2)
-base_dir = Path('code')
 
 
 class Run:
@@ -16,15 +14,13 @@ class Run:
         self.__src: Path = Path(src)
         if self.__src.suffix == '.cpp':
             self.__dst = self.__src.with_suffix('.exe')
-            subprocess.run(['g++', '-std=c++17', self.__src, '-o', self.__dst])
+            if not self.__dst.exists():
+                subprocess.run(
+                    ['g++', '-std=c++17', self.__src, '-o', self.__dst])
             self.__is_cpp = True
         else:
             self.__dst = self.__src
             self.__is_cpp = False
-
-    def __del__(self):
-        if self.__is_cpp:
-            self.__dst.unlink(True)
 
     def __str__(self):
         return str(self.__src)
@@ -36,147 +32,176 @@ class Run:
             return subprocess.run(['python', self.__dst], stdin=stdin, stdout=stdout, timeout=timeout, check=True, encoding='utf-8')
 
 
-def red(s):
-    print('\033[91m', s, '\033[0m', sep='')
+class HackAM:
+    def __init__(self, base_dir: str | Path = 'code', thread_count: int = cpu_count() - 3):
+        self.__BASE_DIR: Path = Path(base_dir)
+        self.__THREAD_COUNT: int = thread_count
 
+    def __del__(self):
+        self.rm_exe()
 
-def green(s):
-    print('\033[32m', s, '\033[0m', sep='')
+    def __red(self, s):
+        print('\033[91m', s, '\033[0m', sep='')
 
+    def __green(self, s):
+        print('\033[32m', s, '\033[0m', sep='')
 
-def yellow(s):
-    print('\033[33m', s, '\033[0m', sep='')
+    def __yellow(self, s):
+        print('\033[33m', s, '\033[0m', sep='')
 
+    @func_set_timeout(30)
+    def __start_hack(self, input_file: Run, std_file: Run, hacked_file: Run, hacked_dir: Path):
+        in_file: Path = hacked_dir / "1.in"
+        out_file: Path = hacked_dir / "1.out"
+        res_file: Path = hacked_dir / "1.result"
+        while True:
+            input_file.run(stdout=in_file.open(
+                'w', encoding='utf-8')).check_returncode()
 
-def NowCoder(contest_id: int | str, cookie: str):
-    table_url = f'https://ac.nowcoder.com/acm-heavy/acm/contest/status-list?statusTypeFilter=5&id={contest_id}&page='
-    js = get(table_url).json()
-    page_cnt = js['data']['basicInfo']['pageCount']
+            std_file.run(in_file.open('r', encoding='utf-8'),
+                         out_file.open("w", encoding='utf-8')).check_returncode()
 
-    submissions = {}
-    for i in range(page_cnt):
-        cur_url = table_url + str(i + 1)
-        js = get(cur_url).json()
-        for sub in js['data']['data']:
-            lang = sub['languageCategoryName']
-            if lang == 'C++':
-                lang = 'cpp'
-            elif lang == 'Python3' or lang == 'PyPy3':
-                lang = 'py'
-            else:
-                lang = 'other'
-            submissions.setdefault((sub['index'], lang), []).append(
-                str(sub['submissionId']))
+            hacked_file.run(in_file.open('r', encoding='utf-8'),
+                            res_file.open("w", encoding='utf-8')).check_returncode()
 
-    base_dir.mkdir(exist_ok=True)
+            with out_file.open("r", encoding='utf-8') as f:
+                out = [line.strip() for line in f.readlines()]
+            with res_file.open("r", encoding='utf-8') as f:
+                result = [line.strip() for line in f.readlines()]
 
-    def crawler(hacked_dir: Path):
-        hacked_dir.mkdir(exist_ok=True)
+            if out != result:
+                break
 
-        submission_url = 'https://ac.nowcoder.com/acm/contest/view-submission?submissionId=' + hacked_dir.name
-        res = get(submission_url, cookies={'t': cookie})
-        soup = BeautifulSoup(res.text, 'html.parser')
+    def __run_hack(self, input_file: Run, std_file: Run, hacked_file: Run, hacked_dir: Path):
+        self.__green(f'Hacking: {input_file} {std_file} {hacked_file}')
+        try:
+            self.__start_hack(input_file, std_file, hacked_file, hacked_dir)
+        except FunctionTimedOut:
+            self.__green(f"Accepted: {hacked_file}")
+            for data in hacked_dir.glob('1.*'):
+                data.unlink()
+        except Exception as e:
+            self.__red(f'Error occurred: {hacked_file} {e}')
+        else:
+            self.__red(f'Hack successfully: {hacked_file}')
 
-        with (hacked_dir / f'hacked.{hacked_dir.parent.name}').open('w', encoding='utf-8') as f:
-            f.write(soup.find('pre').text)
+    def __get_typed_path(self, dir: Path, name: str | Path) -> Path:
+        path = (dir / name).with_suffix('.cpp')
+        if not path.exists():
+            path = path.with_suffix('.py')
+            if not path.exists():
+                return None
+        return path
 
-    with ThreadPoolExecutor(THREAD_COUNT) as executor:
-        res = []
-        for types, ids in submissions.items():
-            problem, lang = types
-            lang_dir: Path = base_dir / problem / lang
-            lang_dir.mkdir(parents=True, exist_ok=True)
-            for id in ids:
-                res.append(executor.submit(crawler, lang_dir / id))
-        wait(res)
+    def process_submission(self, table: Iterable, get_code: Callable[[str], None]):
+        self.__BASE_DIR.mkdir(exist_ok=True)
+        with ThreadPoolExecutor(self.__THREAD_COUNT) as executor:
+            res: list[Future] = []
+            for problem_id, lang, submission_id in table():
+                problem_dir: Path = self.__BASE_DIR / problem_id
+                lang_dir: Path = problem_dir / lang
+                hacked_dir: Path = lang_dir / submission_id
+                hacked_file: Path = hacked_dir / f'hacked.{lang}'
 
+                hacked_dir.mkdir(exist_ok=True, parents=True)
+                with hacked_file.open('w', encoding='utf-8') as f:
+                    f.write(get_code(submission_id))
 
-@func_set_timeout(30)
-def start_hack(input_file: Run, std_file: Run, hacked_file: Run, hacked_dir: Path):
-    in_file = hacked_dir / "1.in"
-    out_file = hacked_dir / "1.out"
-    res_file = hacked_dir / "1.result"
-    while True:
-        input_file.run(stdout=in_file.open(
-            'w', encoding='utf-8')).check_returncode()
-
-        std_file.run(in_file.open('r', encoding='utf-8'),
-                     out_file.open("w", encoding='utf-8')).check_returncode()
-
-        hacked_file.run(in_file.open('r', encoding='utf-8'),
-                        res_file.open("w", encoding='utf-8')).check_returncode()
-
-        with out_file.open("r", encoding='utf-8') as f:
-            out = [line.strip() for line in f.readlines()]
-        with res_file.open("r", encoding='utf-8') as f:
-            result = [line.strip() for line in f.readlines()]
-
-        if out != result:
-            break
-
-
-def run_hack(input_file: Run, std_file: Run, hacked_file: Run, hacked_dir: Path):
-    green(f'Hacking: {input_file} {std_file} {hacked_file}')
-    try:
-        start_hack(input_file, std_file, hacked_file, hacked_dir)
-    except FunctionTimedOut:
-        green(f"Accepted: {hacked_file}")
-        for data in hacked_dir.glob('1.*'):
-            data.unlink()
-    except Exception as e:
-        red(f'Error occurred: {hacked_file} {e}')
-    else:
-        red(f'Hack successfully: {hacked_file}')
-
-
-def process_submissions():
-    with ThreadPoolExecutor(THREAD_COUNT) as executor:
-        res = []
-        for x_path in base_dir.iterdir():
-            # 尝试定义 input.cpp 文件路径
-            input_file = x_path / "input.cpp"
-            if not input_file.exists():
-                input_file = x_path / "input.py"
-                if not input_file.exists():
-                    yellow(f"No input files for {x_path.name}, skipping...")
+                # 尝试定义 input.cpp 文件路径
+                input_file = self.__get_typed_path(problem_dir, 'input')
+                if not input_file:
+                    self.__yellow(
+                        f"No input files for {problem_dir.name}, skipping...")
                     continue
 
-            # 尝试定义 std.cpp 文件路径
-            std_file = x_path / "std.cpp"
-            if not std_file.exists():
-                std_file = x_path / "std.py"
-                if not std_file.exists():
-                    yellow(f"No std files for {x_path.name}, skipping...")
+                # 尝试定义 std.cpp 文件路径
+                std_file = self.__get_typed_path(problem_dir, 'std')
+                if not std_file:
+                    self.__yellow(
+                        f"No std files for {problem_dir.name}, skipping...")
                     continue
 
-            input_file = Run(input_file)
-            std_file = Run(std_file)
+                input_file = Run(input_file)
+                std_file = Run(std_file)
+                hacked_file = Run(hacked_file)
 
-            # 遍历 cpp 目录, hack
-            for lang in ("cpp", 'py'):
-                type_dir = x_path / lang
-                if not type_dir.exists():
+                res.append(executor.submit(self.__run_hack,
+                           input_file, std_file, hacked_file, hacked_dir))
+
+            wait(res)
+
+    def NowCoder(self, contest_id: int | str, t: str):
+        def table():
+            table_url = f'https://ac.nowcoder.com/acm-heavy/acm/contest/status-list?statusTypeFilter=5&id={contest_id}&page='
+            json = get(table_url).json()
+            page_cnt = json['data']['basicInfo']['pageCount']
+
+            for i in range(page_cnt):
+                cur_url = table_url + str(i + 1)
+                json = get(cur_url).json()
+                for sub in json['data']['data']:
+                    lang = sub['languageCategoryName']
+                    if lang == 'C++':
+                        lang = 'cpp'
+                    elif lang == 'Python3' or lang == 'PyPy3':
+                        lang = 'py'
+                    else:
+                        continue
+                    yield sub['index'], lang, str(sub['submissionId'])
+
+        def get_code(submission_id: str):
+            submission_url = 'https://ac.nowcoder.com/acm/contest/view-submission?submissionId=' + submission_id
+            res = get(submission_url, cookies={'t': t})
+            soup = BeautifulSoup(res.text, 'html.parser')
+            return soup.find('pre').text
+
+        self.process_submission(table, get_code)
+
+    def Codeforces(self, contest_id: int | str, csrf_token: str, jsession_id: str):
+        def table():
+            table_url = f'https://codeforces.com/api/contest.status?contestId={contest_id}'
+            json = get(table_url).json()
+            for submission in json['result']:
+                if submission['verdict'] != 'OK':
                     continue
-                for hacked_dir in type_dir.iterdir():
-                    for hacked_file in hacked_dir.glob(f'*.{lang}'):
-                        res.append(executor.submit(
-                            run_hack, input_file, std_file, Run(hacked_file), hacked_dir))
 
-        wait(res)
+                lang: str = submission['programmingLanguage']
+                if lang.startswith('C++'):
+                    lang = 'cpp'
+                elif lang.startswith(('Python', 'PyPy')):
+                    lang = 'py'
+                else:
+                    continue
 
+                yield submission['problem']['index'], lang, str(submission['id'])
 
-def rm_hacked_files():
-    for x_path in base_dir.iterdir():
-        for dirs in x_path.iterdir():
-            if dirs.is_dir():
-                shutil.rmtree(dirs)
+        def get_code(submission_id: str):
+            submission_url = f'https://codeforces.com/data/submitSource?submissionId={submission_id}'
+            header = {
+                'Referer': f'https://codeforces.com/contest/{contest_id}/status',
+                'X-Csrf-Token': csrf_token,
+                'cookie': f'JSESSIONID={jsession_id}',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0',
+            }
+            self.__red(f'pull: {contest_id}')
+            res = post(submission_url, headers=header).json()
+            return res['source']
 
+        self.process_submission(table, get_code)
 
-def rm_exe():
-    for exe in base_dir.rglob('*.exe'):
-        exe.unlink()
+    def rm_hacked_files(self):
+        for x_path in self.__BASE_DIR.iterdir():
+            for dirs in x_path.iterdir():
+                if dirs.is_dir():
+                    shutil.rmtree(dirs)
+
+    def rm_exe(self):
+        for exe in self.__BASE_DIR.rglob('*.exe'):
+            exe.unlink()
 
 
 if __name__ == "__main__":
-    # NowCoder(82707, 'BD68172701F0DDCE982E12856DC560AB')
-    process_submissions()
+    am = HackAM('cf')
+    # am.NowCoder(82707, 'A4965E404E764FB0596C7BE8B815FAEF')
+    am.Codeforces(1977, 'b848e4db925f899d2fb0b33c99c08005',
+                  '176B6491C720AB5BBB95BD6EF7E0AFA0')
